a66bbc24928954ab0eba923ac0f3fb48abea911d
maranan.312474 <maranan.312474@LAB-B-PC02>
2024-02-16 09:15:26 +0800

    Add project files.

---
+++ DataTypesApp/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace DataTypesApp
+{
+    internal class DataTypesProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the Pieces of Apple: ");
+            int Applepcs = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the total price of " + Applepcs + "apple(s): ");
+            double TotalPrice = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("The Total price of " + Applepcs + "apple(s) " + TotalPrice);
+            Console.WriteLine("The value of original price is " + TotalPrice);
+            int c_totalprice = (int)TotalPrice;
+            Console.WriteLine("The value of converted price is " + c_totalprice);
+
+
+        }
+    }
+}
